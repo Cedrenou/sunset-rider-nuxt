2dd2d3f9bfe582667dbb5b79201b3558751b2207
cedricrenouleau <ced241@gmail.com>
2024-08-24 00:07:53 +0200

    mise en place de l'architecture

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
 <template>
+  <header>
+    <nav style="display: flex; flex-direction: column">
+      <a href="/">Home</a>
+      <a href="/products">Products</a>
+      <a href="/products/1">Detail produit</a>
+    </nav>
+  </header>
+  <NuxtPage />
+
+  <footer>
+    <p>Footer</p>
+  </footer>
 </template>
-
-
-<script setup></script>
-
-
-<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      wooCommerceAPIBaseURL: process.env.WOOCOMMERCE_API_BASE_URL,
+    },
+    private: {
+      wooCommerceAPIConsumerKey: process.env.WOOCOMMERCE_API_CONSUMER_KEY,
+      wooCommerceAPIConsumerSecret: process.env.WOOCOMMERCE_API_CONSUMER_SECRET,
+    },
+  }
 })
