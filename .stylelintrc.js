a17d1537c57021a967c85b4c7d19dba04eabeaea
cedricrenouleau <ced241@gmail.com>
2024-08-25 19:51:59 +0200

    add many lib for code quality

---
+++ .stylelintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-recommended-vue'
+    ],
+    rules: {
+        'at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: [
+                    'tailwind',
+                    'apply',
+                    'variants',
+                    'responsive',
+                    'screen'
+                ]
+            }
+        ],
+        'declaration-block-trailing-semicolon': null,
+        'no-descending-specificity': null
+    }
+}
