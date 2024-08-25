5075bf52912b093cf55958565ba303c4010b1549
cedricrenouleau <ced241@gmail.com>
2024-08-25 23:08:45 +0200

    config tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-    "./app.vue",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+    './app.vue',
   ],
   theme: {
+    colors: {
+      white: '#FFFFFF',
+      black: '#000000',
+      primary: {
+        100: '#FFB704',
+        200: '#FF8F11',
+        300: '#FF8F11',
+        400: '#FF5026',
+        500: '#FD322D',
+      },
+      secondary: {
+        100: '#AACEC0',
+        200: '#95BBA2',
+        300: '#7FA48A',
+        400: '#6A8D7E',
+        500: '#548176',
+      },
+    },
     extend: {},
   },
   plugins: [],
